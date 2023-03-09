c53faca95a83b8c65dd5e8c87e9f11d677dc7059
Ishan4u <mohamedishan44@gmail.com>
2023-03-09 19:14:25 +0530

    .

--- laravel-projects/ourmainapp/routes/web.php
+++ laravel-projects/ourmainapp/routes/web.php
@@ -16,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// User related routes
 Route::get('/', [userController::class, "showCorrectHomepage"]);
-
 Route::post('/register', [userController::class, 'register']);
 Route::post('/login', [userController::class, 'login']);
 Route::post('/logout', [userController::class, 'logout']);
+
+// Blog post related routes
+Route
