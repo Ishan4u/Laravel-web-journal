e7d41f612ab22665fa356f101ec2f84944da276e
Ishan4u <mohamedishan44@gmail.com>
2023-03-10 10:20:01 +0530

    Store 'create-post' Form to database <Complete>

--- laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
+++ laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -17,6 +18,8 @@ class PostController extends Controller
         $incomingFields['title'] = strip_tags($incomingFields['title']);
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
         
         return 'hii';
     }
