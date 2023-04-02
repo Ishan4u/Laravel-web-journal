dad0ff86503e95d50a28775747b8fc147e128a52
Ishan4u <mohamedishan44@gmail.com>
2023-04-02 11:46:08 +0530

    Using middleware, prevent UNAUTHORIZED ACTION

--- laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
+++ laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
@@ -10,11 +10,8 @@ class PostController extends Controller
 {
     //
     public function delete(Post $post) {
-        if(auth()->user()->cannot('delete', $post)) {
-            return 'You cannot do that';
-        }
+        
         $post->delete();
-
         return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
     }
 
