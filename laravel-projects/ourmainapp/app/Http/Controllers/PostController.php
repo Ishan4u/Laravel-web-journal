f10e4e9c2f89c662c30da347b215b2e0e8cb9720
Ishan4u <mohamedishan44@gmail.com>
2023-04-02 07:30:41 +0530

    .

--- laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
+++ laravel-projects/ourmainapp/app/Http/Controllers/PostController.php
@@ -10,15 +10,13 @@ class PostController extends Controller
 {
     //
     public function viewSinglePost(Post $post) {
-        if ($post->user_id == auth()->user()->id){
-            return 'you are the author';
-        }
-        return 'you are not author';
+        
         $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3><br>');
         return view('single-post', ['post' => $post]);
     }
 
-    public function storeNewPost(Request $request) {
+    public function storgh auth logout --all
+    eNewPost(Request $request) {
         $incomingFields = $request->validate([
             'title' => 'required',
             'body' => 'required'
