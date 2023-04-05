737386544eec5951439e2c5af6a4f32ece65be4a
Ishan4u <mohamedishan44@gmail.com>
2023-04-05 11:46:28 +0530

    Add isadmin Column to Database

--- laravel-projects/ourmainapp/database/migrations/2023_04_05_061037_add_isadmin_to_users_table.php
+++ laravel-projects/ourmainapp/database/migrations/2023_04_05_061037_add_isadmin_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->boolean('isadmin')->default(0);
         });
     }
 
@@ -23,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('isadmin');
         });
     }
 };
