5eec34f62cdef0aff2dc1cee2ea2487673ad6a9d
Ishan4u <mohamedishan44@gmail.com>
2023-04-08 10:07:57 +0530

    post owner avatar img

--- laravel-projects/ourmainapp/resources/views/profile-posts.blade.php
+++ laravel-projects/ourmainapp/resources/views/profile-posts.blade.php
@@ -20,7 +20,7 @@
         <div class="list-group">
           @foreach ($posts as $post)
           <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
-            <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
+            <img class="avatar-tiny" src="{{$post->user->avatar}}" />
             <strong>{{$post->title}} </strong> {{$post->created_at->format('n/j/Y')}}
           </a>
           @endforeach
