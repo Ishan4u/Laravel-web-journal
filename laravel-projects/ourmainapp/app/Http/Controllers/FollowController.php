8637bb0a3ab8779a1167be7299e9f24e392be957
Ishan4u <mohamedishan44@gmail.com>
2023-04-15 05:13:15 +0530

    follow condition - Follow::where() #learnmore

--- laravel-projects/ourmainapp/app/Http/Controllers/FollowController.php
+++ laravel-projects/ourmainapp/app/Http/Controllers/FollowController.php
@@ -11,7 +11,16 @@ class FollowController extends Controller
     //
     public function createFollow(User $user) {
         // you cannot follow your self
+        if ($user->id == auth()->user()->id) {
+            return back()->with('failure', 'You cannot follow yourself');
+        }
+
         // you connot following someone you're following
+        $existCheck = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
+
+        if($existCheck) {
+            return back()->with('failure', 'You are already following that user');
+        }
 
         $newFollow = new Follow;
         $newFollow->user_id = auth()->user()->id;
