4016238370921ee97994ccad2b762bf6749d38b1
Ishan4u <mohamedishan44@gmail.com>
2023-06-02 22:16:57 +0530

    Add test page and test css

--- laravel-projects/ourmainapp/resources/views/test.blade.php
+++ laravel-projects/ourmainapp/resources/views/test.blade.php
@@ -1,3 +1,109 @@
-<?php
- echo 'test';
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="/test.css" />
+</head>
+
+<body>
+    <div class="container">
+        <h1>Hello, world!</h1>
+    </div>
+
+    {{-- <========= Boosrap Card Section Starts =========> --}}
+    <div class="cream-bg">
+        <div class="container">
+            <div class="row g-5 justify-content-center">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="row g-0">
+                            <div class="col-6 col-md-5">
+                                <img class="card-img img-fluid rounded-start"
+                                    src="https://www.1zoom.me/prev/297/296865.jpg" alt="">
+                            </div>
+                            <div class="col-6 col-md-7">
+                                <div class="card-body d-flex flex-column">
+                                    <div class="h-100">
+                                        <h3 class="card-title">Ishan</h3>
+                                        <p class="card-text">
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                            Lorem Ipsum has been the industry's standard dummy text ever since the
+                                            1500s,
+                                        </p>
+                                        {{-- <h4 class="card-titile mb-3">views<strong>30</strong></h4> --}}
+                                    </div>
+                                    <div style="margin-top:.7rem;">
+                                        <button class="btn btn-dark"> view</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <========= Boosrap Card Section Ends   =========> --}}
+
+    {{-- <=====Boostrap Card image Zoomin Zoomout Starts=====> --}}
+    <div class="container py-md-5 container--narrow" style="margin-top: 8%;">
+        <div class="row justify-content-center">
+            <div class="card-deck">
+                <div class="card">
+                  <img class="card-img-top" src="https://www.1zoom.me/prev/297/296865.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </div>
+                </div>
+                <div class="card">
+                  <img class="card-img-top" src="https://www.1zoom.me/prev/297/296865.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </div>
+                </div>
+                <div class="card">
+                  <img class="card-img-top" src="https://www.1zoom.me/prev/297/296865.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                  </div>
+                </div>
+              </div>
+        </div>
+    </div>
+    {{-- <=====Boostrap Card image Zoomin Zoomout Ends=====> --}}
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
